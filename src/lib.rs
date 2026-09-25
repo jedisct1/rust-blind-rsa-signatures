640084d82abd5bbe48c65c3d8e6f647c8963ec73
Frank Denis <github@pureftpd.org>
2026-09-25 12:38:45 +0200

    Fix stale documentation links

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Author-blinded RSASSA-PSS RSAE signatures.
 //!
-//! This is an implementation of the [RSA Blind Signatures](https://www.rfc-editor.org/rfc/rfc9474.html) (RFC 9474), based on [the Zig implementation](https://github.com/jedisct1/zig-rsa-blind-signatures).
+//! This is an implementation of the [RSA Blind Signatures](https://www.rfc-editor.org/rfc/rfc9474.html) (RFC 9474), based on [the Zig implementation](https://github.com/jedisct1/zig-blind-rsa-signatures).
 //!
 //! ```rust
 //! use blind_rsa_signatures::{KeyPair, Sha384, PSS, Randomized, DefaultRng};
